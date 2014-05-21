5e2eb495970c32c126e8d63ee411788f15ecb27a
Thien-Thi Nguyen <ttn@gnu.org>
2014-05-21 09:34:22 +0200

    [aa2u maint] Add ‘Maintainer’ header per top-level README; nfc.

--- ascii-art-to-unicode.el
+++ ascii-art-to-unicode.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2014  Free Software Foundation, Inc.
 
 ;; Author: Thien-Thi Nguyen <ttn@gnu.org>
+;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Version: 1.7
 
 ;; This program is free software; you can redistribute it and/or modify
