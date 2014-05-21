4e951353bdb5b3527edc4b7a01b9db58126cd058
Thien-Thi Nguyen <ttn@gnu.org>
2014-05-21 09:36:58 +0200

    [aa2u] Declare package keywords.
    * packages/ascii-art-to-unicode/ascii-art-to-unicode.el [Keywords]: New header.

--- ascii-art-to-unicode.el
+++ ascii-art-to-unicode.el
@@ -5,6 +5,7 @@
 ;; Author: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Version: 1.7
+;; Keywords: ascii, unicode, box-drawing
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
