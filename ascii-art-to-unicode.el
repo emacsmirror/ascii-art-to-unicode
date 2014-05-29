78f8a2d12682334ddfff309ba4ee72985236d15f
Thien-Thi Nguyen <ttn@gnu.org>
2014-05-29 12:12:42 +0200

    [aa2u] Update homepage; drop other links.
    * packages/ascii-art-to-unicode/ascii-art-to-unicode.el [URL]: New header.
    [Commentary]: Remove the HACKING and Tip Jar links.

--- ascii-art-to-unicode.el
+++ ascii-art-to-unicode.el
@@ -6,6 +6,7 @@
 ;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Version: 1.8
 ;; Keywords: ascii, unicode, box-drawing
+;; URL: http://www.gnuvola.org/software/aa2u/
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -92,11 +93,6 @@
 ;;            """""""""
 ;;
 ;; Command `aa2u-mark-rectangle-as-text' is similar, for rectangles.
-;;
-;;
-;; See Also
-;; - HACKING: <http://git.sv.gnu.org/cgit/emacs/elpa.git/tree/packages/ascii-art-to-unicode/HACKING>
-;; - Tip Jar: <http://www.gnuvola.org/software/aa2u/>
 
 ;;; Code:
 
