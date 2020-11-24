83ec71af9e1812df781b78f28fa9dac0ff7b61bc
Thien-Thi Nguyen <ttn@gnu.org>
2020-11-24 03:41:50 -0500

    [aa2u] Release: 1.13
    * packages/ascii-art-to-unicode/ascii-art-to-unicode.el
    [Version]: Bump to "1.13".

--- ascii-art-to-unicode.el
+++ ascii-art-to-unicode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
-;; Version: 1.12
+;; Version: 1.13
 ;; Keywords: ascii, unicode, box-drawing
 ;; URL: http://www.gnuvola.org/software/aa2u/
 
